36795598b904379203ec15d736e7a6272845779b
AlExito745 <alxtinoco1988@gmail.com>
2023-01-13 20:43:37 -0800

    update betty style

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,8 +1,21 @@
 #include "binary_trees.h"
 
-binary_tree_t *binary_tree_uncle(binary_tree_t *node) 
+/**
+ * binary_tree_uncle - Finds the uncle of a node
+ * @node: Pointer to node to find uncle
+ *
+ * Return: Uncle node, otherwise NULL
+ */
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-    if (!node || !node->parent || !node->parent->parent) return NULL;
-    if (node->parent->parent->left == node->parent) return node->parent->parent->right;
-    return node->parent->parent->left;
+	if (!node || !node->parent || !node->parent->parent)
+	{
+		return (NULL);
+	}
+	if (node->parent->parent->left == node->parent)
+	{
+		return (node->parent->parent->right);
+	}
+	return (node->parent->parent->left);
 }
