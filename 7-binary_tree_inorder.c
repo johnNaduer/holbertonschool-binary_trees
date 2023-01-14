2cf4dca24001d34757f24cc253747a2b94a634cd
AlExito745 <alxtinoco1988@gmail.com>
2023-01-13 19:32:24 -0800

    update betty style

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,10 +1,22 @@
 #include "binary_trees.h"
 
-void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int)) 
-{
-    if (!tree || !func) return;
+/**
+ * binary_tree_inorder - Goes through a binary tree using in-order traversal
+ * @tree: Pointer to the root node of the tree to traverse
+ * @func:Pointer to a function to call for each node
+ *
+ * Value in the node must be passed as a function parameter
+ * Do nothing if tree or func NULL
+ */
 
-    binary_tree_inorder(tree->left, func);
-    func(tree->n);
-    binary_tree_inorder(tree->right, func);
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+	{
+		return;
+	}
+
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
 }
