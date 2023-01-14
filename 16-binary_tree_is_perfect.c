65f3152d0515e8aaf25819deb55e108a1625b89a
AlExito745 <alxtinoco1988@gmail.com>
2023-01-13 20:53:37 -0800

    update betty style

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,12 +1,66 @@
 #include "binary_trees.h"
 
-int binary_tree_is_perfect(const binary_tree_t *tree) 
+/**
+ * _pow_recursion - Calculate x raised to the power of y
+ * @x: Base
+ * @y: Exponent
+ *
+ * Return: x raised to power of y
+ */
+
+int _pow_recursion(int x, int y)
 {
-    if (!tree) return 0;
-    int left_depth = binary_tree_depth(tree->left);
-    int right_depth = binary_tree_depth(tree->right);
-    if (left_depth != right_depth) return 0;
-    if (!tree->left && !tree->right) return 1;
-    if (!tree->left || !tree->right) return 0;
-    return binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right);
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
+
+/**
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree to measure the height
+ *
+ * Return: Height of tree, 0 if NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t height_l;
+	size_t height_r;
+
+	if (!tree)
+		return (0);
+
+	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return (height_l > height_r ? height_l : height_r);
+}
+
+/**
+ * binary_tree_size - Measures the size of a binary tree
+ * @tree: Pointer to root node of tree to measure size
+ *
+ * Return: Size of binary tree, 0 if tree is NULL
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+}
+
+/**
+ * binary_tree_is_perfect - Determine if binary tree is perfect
+ * @tree: Pointer to root of tree
+ *
+ * Return: 1 if tree is perfect, 0 otherwise
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	int nodes, target, height;
+
+	height = binary_tree_height(tree) + 1;
+	nodes = binary_tree_size(tree);
+	target = _pow_recursion(2, height);
+	return (target - 1 == nodes);
 }
