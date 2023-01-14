4892c9bdf8602a52a8450eefab98e05ed6d26ba3
AlExito745 <alxtinoco1988@gmail.com>
2023-01-13 20:14:12 -0800

    update betty style

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,7 +1,17 @@
 #include "binary_trees.h"
 
-size_t binary_tree_size(const binary_tree_t *tree) 
+/**
+ * binary_tree_size - Measures the size of a binary tree
+ * @tree: Pointer to root node of tree to measure size
+ *
+ * Return: Size of binary tree, 0 if tree is NULL
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
 {
-    if (!tree) return 0;
-    return 1 + binary_tree_size(tree->left) + binary_tree_size(tree->right);
+	if (!tree)
+	{
+		return (0);
+	}
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
 }
