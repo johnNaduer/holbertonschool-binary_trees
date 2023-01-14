cddeffbd6b1c893342a848948aaa3bb113293c4e
AlExito745 <alxtinoco1988@gmail.com>
2023-01-13 20:39:22 -0800

    update betty style

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,8 +1,21 @@
 #include "binary_trees.h"
 
-binary_tree_t *binary_tree_sibling(binary_tree_t *node) 
+/**
+ * binary_tree_sibling - Finds siblings of a node
+ * @node: Pointer to node to find the sibling
+ *
+ * Return: Sibling of node, otherwise NULL
+ */
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-    if (!node || !node->parent) return NULL;
-    if (node->parent->left == node) return node->parent->right;
-    return node->parent->left;
+	if (!node || !node->parent)
+	{
+		return (NULL);
+	}
+	if (node->parent->left == node)
+	{
+		return (node->parent->right);
+	}
+	return (node->parent->left);
 }
