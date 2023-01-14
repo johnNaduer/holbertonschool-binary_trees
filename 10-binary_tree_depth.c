edc8c0583e3cd0f6c63dc8e535c622c29c920f48
AlExito745 <alxtinoco1988@gmail.com>
2023-01-13 20:06:10 -0800

    update betty style

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -6,15 +6,7 @@
  *
  * Return: Depth of binary tree, 0 if NULL
  */
-
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	if (!tree)
-	{
-		return (0);
-	}
-	size_t left_depth = binary_tree_depth(tree->left);
-	size_t right_depth = binary_tree_depth(tree->right);
-
-	return (1 + (left_depth > right_depth ? left_depth : right_depth));
+	return (tree && tree->parent ? 1 + binary_tree_depth(tree->parent) : 0);
 }
