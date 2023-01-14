ea27d996f457aa4e75e1e047d0b57567b345c8e7
AlExito745 <alxtinoco1988@gmail.com>
2023-01-13 19:43:45 -0800

    update betty style

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,15 +1,36 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree to measure the height
+ *
+ * Return: Height of tree, 0 if NULL
+ */
+
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t left_height, right_height;
+	size_t left_height, right_height;
 
-    if (!tree) return 0;
-    if (!tree->left && !tree->right) return 0;
+	if (!tree)
+	{
+		return (0);
+	}
 
-    left_height = binary_tree_height(tree->left);
-    right_height = binary_tree_height(tree->right);
+	if (!tree->left && !tree->right)
+	{
+		return (0);
+	}
 
-    if (left_height > right_height) return (left_height + 1);
-    else return (right_height + 1);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	if (left_height > right_height)
+	{
+		return (left_height + 1);
+	}
+
+	else
+	{
+		return (right_height + 1);
+	}
 }
