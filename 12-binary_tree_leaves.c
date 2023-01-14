7b40b8b81f9a4266505ebe28b5d50e923505ad1d
AlExito745 <alxtinoco1988@gmail.com>
2023-01-13 20:19:18 -0800

    update betty style

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,8 +1,22 @@
 #include "binary_trees.h"
 
-size_t binary_tree_leaves(const binary_tree_t *tree) 
+/**
+ * binary_tree_leaves - Count number of leaves in binary tree
+ * @tree: Pointer to root of binary tree
+ *
+ * Return: Number of leaves in binary tree
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-    if (!tree) return 0;
-    if (!tree->left && !tree->right) return 1;
-    return binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
+	if (!tree)
+	{
+		return (0);
+	}
+
+	if (!tree->left && !tree->right)
+	{
+		return (1);
+	}
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
